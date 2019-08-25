3028cf3f9e92b1d2a4a1a2f28eefb687b8eef355
YGT-cxy <1159514579@qq.com>
2019-08-25 15:52:12 +0800

    feat:

--- examples/interceptor/app.ts
+++ examples/interceptor/app.ts
@@ -1,68 +1,41 @@
 import axios from '../../src/index'
 
-// axios.interceptors.request.use(config => {
-//   config.headers.test += '1'
-//   return config
-// })
-// axios.interceptors.request.use(config => {
-//   config.headers.test += '2'
-//   return config
-// })
-// axios.interceptors.request.use(config => {
-//   config.headers.test += '3'
-//   return config
-// })
-
-// axios.interceptors.response.use(res => {
-//   res.data += '1'
-//   return res
-// })
-// let interceptor = axios.interceptors.response.use(res => {
-//   res.data += '2'
-//   return res
-// })
-// axios.interceptors.response.use(res => {
-//   res.data += '3'
-//   return res
-// })
-
-// axios.interceptors.response.eject(interceptor)
-
-// axios({
-//   url: '/interceptor/get',
-//   method: 'get',
-//   headers: {
-//     test: ''
-//   }
-// }).then((res) => {
-//   console.log(res.data)
-// }).catch(err => {
-//   console.log(err)
-// })
-
 axios.interceptors.request.use(config => {
-  config.timeout = 2000
+  config.headers.test += '1'
+  return config
+})
+axios.interceptors.request.use(config => {
+  config.headers.test += '2'
+  return config
+})
+axios.interceptors.request.use(config => {
+  config.headers.test += '3'
   return config
 })
 
-const instance = axios.create()
-
-instance.interceptors.request.use(config => {
-  config.timeout = 2000
-  config.withCredentials = true
-  console.log(config)
-  return config
+axios.interceptors.response.use(res => {
+  res.data += '1'
+  return res
+})
+let interceptor = axios.interceptors.response.use(res => {
+  res.data += '2'
+  return res
+})
+axios.interceptors.response.use(res => {
+  res.data += '3'
+  return res
 })
 
-// console.log(instance.interceptors.request)
-// instance({
-//   url: '/interceptor/get'
-// })
+axios.interceptors.response.eject(interceptor)
 
-instance.get('/interceptor/get', {
-  params: {
-    a: 1
+axios({
+  url: '/interceptor/get',
+  method: 'get',
+  headers: {
+    test: ''
   }
-}).then(res => {
-  console.log(res)
+}).then((res) => {
+  console.log(res.data)
+}).catch(err => {
+  console.log(err)
 })
