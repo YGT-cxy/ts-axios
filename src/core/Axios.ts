8b4d63ab19b6b5c2e195abc68f1818d7f9b903e2
YGT-cxy <1159514579@qq.com>
2019-09-08 17:59:27 +0800

    feat:

--- src/core/Axios.ts
+++ src/core/Axios.ts
@@ -9,7 +9,6 @@ import {
 import dispatchRequest, { transformURL } from './dispatchRequest'
 import InterceptorManager from './InterceptorManager'
 import mergeConfig from './mergeConfig'
-import { flattenHeaders } from './../helpers/headers'
 
 // request和response拦截器对象接口
 interface Interceptors {
@@ -55,8 +54,6 @@ export default class Axios {
 
     // 合并默认的config和自定义的config
     config = mergeConfig(this.defaults, config)
-    // 将method大写的转为小写
-    config.method = config.method.toLowerCase()
 
     // 拦截器
     const chain: PromiseChain<any>[] = [
